9c4bcd0f0ef3edd555b6067bef3c0b366dc8cad0
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2019-01-31 08:39:46 +0100

    Isolation - Implemented hibernate listener for catalog events

---
+++ sm-catalog/src/main/java/com/salesmanager/catalog/integration/CatalogDeleteEventListener.java
@@ -0,0 +1,30 @@
+package com.salesmanager.catalog.integration;
+
+import com.salesmanager.catalog.model.product.Product;
+import org.hibernate.event.spi.PostDeleteEvent;
+import org.hibernate.event.spi.PostDeleteEventListener;
+import org.hibernate.persister.entity.EntityPersister;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CatalogDeleteEventListener implements PostDeleteEventListener, ApplicationEventPublisherAware {
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public void onPostDelete(PostDeleteEvent event) {
+    }
+
+    @Override
+    public boolean requiresPostCommitHanding(EntityPersister persister) {
+        return false;
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+}
---
+++ sm-catalog/src/main/java/com/salesmanager/catalog/integration/HibernateCatalogListenerConfigurer.java
@@ -0,0 +1,42 @@
+package com.salesmanager.catalog.integration;
+
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+@Component
+public class HibernateCatalogListenerConfigurer {
+
+    @PersistenceUnit(unitName = "shopizerContainer")
+    private EntityManagerFactory entityManagerFactory;
+
+    private CatalogCreateEventListener catalogCreateEventListener;
+
+    private CatalogDeleteEventListener catalogDeleteEventListener;
+
+    private CatalogUpdateEventListener catalogUpdateEventListener;
+
+    @Autowired
+    public HibernateCatalogListenerConfigurer(CatalogCreateEventListener catalogCreateEventListener, CatalogDeleteEventListener catalogDeleteEventListener,
+                                              CatalogUpdateEventListener catalogUpdateEventListener) {
+        this.catalogCreateEventListener = catalogCreateEventListener;
+        this.catalogDeleteEventListener = catalogDeleteEventListener;
+        this.catalogUpdateEventListener = catalogUpdateEventListener;
+    }
+
+
+    @PostConstruct
+    protected void init() {
+        SessionFactoryImpl sessionFactory = entityManagerFactory.unwrap(SessionFactoryImpl.class);
+        EventListenerRegistry registry = sessionFactory.getServiceRegistry().getService(EventListenerRegistry.class);
+        registry.getEventListenerGroup(EventType.POST_INSERT).appendListener(catalogCreateEventListener);
+        registry.getEventListenerGroup(EventType.POST_DELETE).appendListener(catalogDeleteEventListener);
+        registry.getEventListenerGroup(EventType.POST_UPDATE).appendListener(catalogUpdateEventListener);
+    }
+}
