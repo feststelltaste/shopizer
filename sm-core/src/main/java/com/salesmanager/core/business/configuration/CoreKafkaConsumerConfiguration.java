4184bc1a9d365f87c2bf5408d54737b0ebf93be4
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2019-02-19 08:23:56 +0100

    Extraction - Added kafka consumer config to sm-core

---
+++ sm-core/src/main/java/com/salesmanager/core/business/configuration/CoreKafkaConsumerConfiguration.java
@@ -0,0 +1,54 @@
+package com.salesmanager.core.business.configuration;
+
+import com.salesmanager.catalog.api.dto.product.ProductDTO;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class CoreKafkaConsumerConfiguration {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public Map<String, Object> consumerConfiguration() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                environment.getRequiredProperty("core.spring.kafka.bootstrap-servers"));
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                environment.getRequiredProperty("core.spring.kafka.consumer.auto-offset-reset"));
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                environment.getRequiredProperty("core.spring.kafka.consumer.group-id"));
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, ProductDTO> productConsumerFactory() {
+
+        return new DefaultKafkaConsumerFactory<>(consumerConfiguration(), new StringDeserializer(), new JsonDeserializer<>(ProductDTO.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ProductDTO> productKafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, ProductDTO> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(productConsumerFactory());
+        return factory;
+    }
+}
