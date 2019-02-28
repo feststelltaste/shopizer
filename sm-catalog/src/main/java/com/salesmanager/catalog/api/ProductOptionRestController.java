4c6c38ebbde621fa6841ce48c445d858893d3c04
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2019-02-28 09:29:47 +0100

    Extraction - Added rest endpoint for product option

---
+++ sm-catalog/src/main/java/com/salesmanager/catalog/api/ProductOptionRestController.java
@@ -0,0 +1,33 @@
+package com.salesmanager.catalog.api;
+
+import com.salesmanager.catalog.api.dto.product.ProductOptionDTO;
+import com.salesmanager.catalog.business.service.product.attribute.ProductOptionService;
+import com.salesmanager.catalog.model.product.attribute.ProductOption;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/catalog/product-option")
+public class ProductOptionRestController {
+
+    private final ProductOptionService productOptionService;
+
+    @Autowired
+    public ProductOptionRestController(ProductOptionService productOptionService) {
+        this.productOptionService = productOptionService;
+    }
+
+    @RequestMapping(path = "/{optionId}", method = RequestMethod.GET)
+    public ResponseEntity<?> getProductOption(@PathVariable("optionId") Long optionId) {
+        ProductOption productOption = productOptionService.getById(optionId);
+        if (productOption != null) {
+            ProductOptionDTO dto = productOption.toDTO();
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
